8fca632aa46f699b365e018ff0cf542b8e567ddb
Leolfc <leonardofeliciano81@gmail.com>
2025-03-04 09:19:04 -0300

    adicionando paginação

--- src/components/CardPost/index.jsx
+++ src/components/CardPost/index.jsx
@@ -1,26 +1,26 @@
 import Image from "next/image";
 import { Avatar } from "../Avatar";
-import styles from "./Card.module.css"
+import styles from "./Card.module.css";
+
 export const CardPost = ({ post }) => {
   return (
-    <article className={styles.article} >
+    <article className={styles.card}>
       <header className={styles.header}>
         <figure>
           <Image
             src={post.cover}
             width={438}
             height={133}
-            alt={`Capa do poste do de titulo: ${post.title}`}
+            alt={`Capa do post de titulo: ${post.title}`}
           />
         </figure>
       </header>
       <section className={styles.body}>
         <h2>{post.title}</h2>
         <p>{post.body}</p>
-        texto
       </section>
       <footer className={styles.footer}>
-        <Avatar  imageSrc={post.author.avatar} name={post.author.Username} />
+        <Avatar imageSrc={post.author.avatar} name={post.author.username} />
       </footer>
     </article>
   );
